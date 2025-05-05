2ae410d55a990a7369bdfe5fbe0d8b8856dc0339
jackasianboy <nrezafahlevi@gmail.com>
2025-05-05 09:31:50 +0700

    fix error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,8 @@
-// app/layout.tsx
-import { type Metadata } from 'next'
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/nextjs'
-
-import { Geist, Geist_Mono } from 'next/font/google'
+import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
 
 import './globals.css'
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -32,40 +12,21 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
   title: 'Evently',
-  description: 'Evently is a platform for event management',
+  description: 'Evently is a platform for event management.',
   icons: {
-    icon: '/favicon.ico',
-  },
+    icon: '/assets/images/logo.svg'
+  }
 }
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body
-          className={`
-            ${geistSans.variable}
-            ${geistMono.variable}
-            ${poppins.variable}
-            font-sans antialiased
-          `}
-        >
-          {/* <header className="flex justify-end items-center p-4 gap-4 h-16 border-b border-gray-200">
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton afterSignOutUrl="/" />
-            </SignedIn>
-          </header> */}
-
-          {children}
-        </body>
+        <body className={poppins.variable}>{children}</body>
       </html>
     </ClerkProvider>
   )
